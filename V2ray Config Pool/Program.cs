6bf67534120ad26f97255e0c5faaa78f792f933c
GitDev90 <gitdev90@gmail.com>
2024-11-23 10:01:39 +0100

    Add files via upload

---
+++ V2ray Config Pool/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using V2ray_Config_Pool.UI;
+
+namespace V2ray_Config_Pool
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Main());
+        }
+    }
+}
